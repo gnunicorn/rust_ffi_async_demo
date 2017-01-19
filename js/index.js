621ab8002110c698b4bd69f6694246bccfb6dfd4
Benjamin Kampmann <ben@gnunicorn.org>
2017-01-19 09:42:20 -0300

    pointer based approach

--- js/index.js
+++ js/index.js
@@ -2,29 +2,20 @@ var ffi = require('ffi'),
     ref = require('ref'),
     arrayType = require('ref-array');
 
-const ourArray = new arrayType(ref.types.uint8, 32);
+const ourArray = arrayType(ref.types.uint8, 32);
 
-
-var lib = ffi.Library('./libffi_async_demo', {
-  'register': [ 'void', [ 'pointer', 'pointer' ] ],
+var lib = ffi.Library('./../target/debug/libffi_async_demo', {
+  // 'register': [ 'void', [ 'pointer', 'pointer' ] ],
   // array testing
   'get_array': [ 'void', [ 'pointer', 'pointer' ] ],
-  'print_array': [ 'void', [ "pointer", ourArray ] ]
+  'print_array': [ 'void', [ 'pointer', ref.refType(ourArray) ] ]
 });
 
-var callback = function() {
-    var state = {x: "tada"};
-
-    return ffi.Callback('void', ['pointer', ref.types.int32], function(ctx, code) {
-        console.log("getting back: ", code);
-        console.log(state.x);
-    })
-}();
-
-lib.register(ref.NULL, callback)
-
-lib.get_array(ref.NULL, ffi.Callback('void', ['pointer', ourArray], function(ctx, a) {
-        console.log("got back ", a);
+var callback = ffi.Callback('void', ['pointer', ref.refType(ourArray)], function(ctx, a) {
+        // console.log("got back ", ref.reinterpret(a.buffer, 32));
+        console.log("got back ", a.hexAddress(), a.deref().length);
+        // console.log("got back ", a.deref());
         lib.print_array(ctx, a);
-    }))
+    });
+lib.get_array(ref.NULL, callback);
 var timeout = setInterval(function() {process.exit()}, 500);
