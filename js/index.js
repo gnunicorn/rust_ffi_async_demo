20dc4e0cc2e69743e045a1066d6405e6420f3bf5
Benjamin Kampmann <ben@gnunicorn.org>
2017-01-19 14:07:29 -0300

    fixup

--- js/index.js
+++ js/index.js
@@ -7,15 +7,14 @@ const ourArray = arrayType(ref.types.uint8, 32);
 var lib = ffi.Library('./../target/debug/libffi_async_demo', {
   // 'register': [ 'void', [ 'pointer', 'pointer' ] ],
   // array testing
-  'get_array': [ 'void', [ 'pointer', 'pointer' ] ],
-  'print_array': [ 'void', [ 'pointer', ref.refType(ourArray) ] ]
+  simple_buffer: [ 'void', ['pointer', 'pointer']]
+  // 'get_array': [ 'void', [ 'pointer', 'pointer' ] ],
+  // 'print_array': [ 'void', [ 'pointer', ref.refType(ourArray) ] ]
 });
 
-var callback = ffi.Callback('void', ['pointer', ref.refType(ourArray)], function(ctx, a) {
-        // console.log("got back ", ref.reinterpret(a.buffer, 32));
-        console.log("got back ", a.hexAddress(), a.deref().length);
-        // console.log("got back ", a.deref());
-        lib.print_array(ctx, a);
+var callback = ffi.Callback('void', ['pointer', ref.refType(ref.types.uint8), ref.types.size_t], function(ctx, b, l) {
+        console.log("got back ", ref.reinterpret(b, l));
     });
-lib.get_array(ref.NULL, callback);
+// lib.get_array(ref.NULL, callback);
+lib.simple_buffer(ref.NULL, callback)
 var timeout = setInterval(function() {process.exit()}, 500);
