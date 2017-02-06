c358b6e9a04e5d63fe84b1755003c92cafb297e1
Benjamin Kampmann <ben@gnunicorn.org>
2017-02-06 18:38:23 +0100

    ref array in struct test

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
 var ffi = require('ffi'),
     ref = require('ref'),
-    arrayType = require('ref-array');
+    arrayType = require('ref-array'),
+    Struct = require('ref-struct');
 
 const ourArray = arrayType(ref.types.uint8, 32);
+
+const File = Struct({
+	name: arrayType(ref.types.uint8, 32)
+});
+
+const FilePtr = ref.refType(File);
+
 
 var lib = ffi.Library('./../target/debug/libffi_async_demo', {
   // 'register': [ 'void', [ 'pointer', 'pointer' ] ],
   // array testing
-  simple_buffer: [ 'void', ['pointer', 'pointer']]
-  // 'get_array': [ 'void', [ 'pointer', 'pointer' ] ],
-  // 'print_array': [ 'void', [ 'pointer', ref.refType(ourArray) ] ]
+  // simple_buffer: [ 'void', ['pointer', 'pointer']]
+  'get_file': [ 'void', [ 'pointer', 'pointer' ] ],
+  'print_file': [ 'void', [ 'pointer', FilePtr ] ]
 });
 
-var callback = ffi.Callback('void', ['pointer', ref.refType(ref.types.uint8), ref.types.size_t], function(ctx, b, l) {
-        console.log("got back ", ref.reinterpret(b, l));
+var callback = ffi.Callback('void', ['pointer', FilePtr], function(ctx, f) {
+		const file = File(f);
+        console.log("got back ", file);
+        lib.print_file(ref.NULL, f);
     });
 // lib.get_array(ref.NULL, callback);
-lib.simple_buffer(ref.NULL, callback)
+lib.get_file(ref.NULL, callback)
 var timeout = setInterval(function() {process.exit()}, 500);
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,15 @@ use std::os::raw::c_void;
 struct OpaqueCtx(*mut c_void);
 unsafe impl Send for OpaqueCtx {}
 
+
+/// FFI-wrapper for `File`.
+#[repr(C)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct File {
+    /// Name of the `ImmutableData` containing the content of this file.
+    pub name: [u8; 32],
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn register(ctx: *mut c_void,
                                   cb: unsafe extern "C" fn(*mut c_void, i32)) {
@@ -19,21 +28,23 @@ pub unsafe extern "C" fn register(ctx: *mut c_void,
 
 
 #[no_mangle]
-pub unsafe extern "C" fn get_array(ctx: *mut c_void,
-                                  cb: extern "C" fn(*mut c_void, *const [u8; 32])) {
+pub unsafe extern "C" fn get_file(ctx: *mut c_void,
+                                  cb: extern "C" fn(*mut c_void, *const File)) {
                                       print!("Updated");
     println!("{:p}", ctx);
     let ctx = OpaqueCtx(ctx);
-    let arr = [208, 39, 232, 3, 0, 0, 0, 0, 160, 221, 98, 168, 254, 127, 0, 0, 152, 28, 159, 78, 196, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99];
-    println!("address {:?}", &arr as *const [u8; 32]);
+    let f = File {
+      name: [208, 39, 232, 3, 0, 0, 0, 0, 160, 221, 98, 168, 254, 127, 0, 0, 152, 28, 159, 78, 196, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99]
+    };
+    println!("file {:?}", f);
     let _ = thread::spawn(move || {
            let ctx = ctx.0;
-           cb(ctx, &arr);
+           cb(ctx, &f);
      });
 }
 
 
 #[no_mangle]
-pub unsafe extern "C" fn print_array(_app: *const c_void, a: *const [u8; 32]) {
-    println!("input is {:?} - {:?}", a, *a);
+pub unsafe extern "C" fn print_file(_app: *const c_void, a: *const File) {
+    println!("input is {:?}", *a);
 }
