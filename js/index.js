623a0743b74e4ff0232fa3160ecc8a4e66fc9036
Benjamin Kampmann <ben@gnunicorn.org>
2017-02-06 19:14:03 +0100

    complex file struct

--- js/index.js
+++ js/index.js
@@ -5,10 +5,31 @@ var ffi = require('ffi'),
 
 const ourArray = arrayType(ref.types.uint8, 32);
 
-const File = Struct({
-	name: arrayType(ref.types.uint8, 32)
+const i32 = ref.types.int32;
+
+const Time = Struct({
+  "tm_sec": i32,
+  "tm_min": i32,
+  "tm_hour": i32,
+  "tm_mday": i32,
+  "tm_mon": i32,
+  "tm_year": i32,
+  "tm_wday": i32,
+  "tm_yday": i32,
+  "tm_isdst": i32,
+  "tm_utcoff": i32,
+  "tm_nsec": i32,
 });
 
+const File = Struct({
+  size: ref.types.uint64,
+  created: Time,
+  modified: Time,
+  user_metadata_ptr: ref.refType(ref.types.uint8),
+  user_metadata_len: ref.types.size_t,
+  user_metadata_cap: ref.types.size_t,
+  data_map_name: arrayType(ref.types.uint8, 32)
+});
 const FilePtr = ref.refType(File);
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 extern crate thread_id;
+extern crate time;
 
 use std::thread;
 use std::os::raw::c_void;
+use time::Tm;
+use std::mem;
+
 
 struct OpaqueCtx(*mut c_void);
 unsafe impl Send for OpaqueCtx {}
@@ -11,8 +15,31 @@ unsafe impl Send for OpaqueCtx {}
 #[repr(C)]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct File {
+    /// File size in bytes.
+    pub size: u64,
+    /// Creation time.
+    pub created: Tm,
+    /// Modification time.
+    pub modified: Tm,
+    /// Pointer to the user metadata.
+    pub user_metadata_ptr: *mut u8,
+    /// Size of the user metadata.
+    pub user_metadata_len: usize,
+    /// Capacity of the user metadata (internal field).
+    pub user_metadata_cap: usize,
     /// Name of the `ImmutableData` containing the content of this file.
-    pub name: [u8; 32],
+    pub data_map_name: [u8; 32],
+}
+
+impl Drop for File {
+    #[allow(unsafe_code)]
+    fn drop(&mut self) {
+        let _ = unsafe {
+            Vec::from_raw_parts(self.user_metadata_ptr,
+                                self.user_metadata_len,
+                                self.user_metadata_cap)
+        };
+    }
 }
 
 #[no_mangle]
@@ -33,14 +60,27 @@ pub unsafe extern "C" fn get_file(ctx: *mut c_void,
                                       print!("Updated");
     println!("{:p}", ctx);
     let ctx = OpaqueCtx(ctx);
+    let mut meta = vec![0, 208, 109];
+
+    let ptr = meta.as_mut_ptr();
+    let len = meta.len();
+    let cap = meta.capacity();
+    mem::forget(meta);
+
     let f = File {
-      name: [208, 39, 232, 3, 0, 0, 0, 0, 160, 221, 98, 168, 254, 127, 0, 0, 152, 28, 159, 78, 196, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99]
+      size: 0,
+      created: time::now_utc(),
+      modified: time::now_utc(),
+      user_metadata_len: len,
+      user_metadata_ptr: ptr,
+      user_metadata_cap: cap,
+      data_map_name: [208, 39, 232, 3, 0, 0, 0, 0, 160, 221, 98, 168, 254, 127, 0, 0, 152, 28, 159, 78, 196, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99]
     };
     println!("file {:?}", f);
-    let _ = thread::spawn(move || {
-           let ctx = ctx.0;
-           cb(ctx, &f);
-     });
+    // let _ = thread::spawn(move || {
+       let ctx = ctx.0;
+       cb(ctx, &f);
+     // });
 }
 
 
